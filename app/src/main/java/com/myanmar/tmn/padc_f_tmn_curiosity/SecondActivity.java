e6f2be778e297d5d09ce988d381997532aaff5ce
DESKTOP-0QB8A7R\msi <thetmyatnoe223@gmail.com>
2017-11-30 00:36:30 +0630

    first time commit

---
+++ app/src/main/java/com/myanmar/tmn/padc_f_tmn_curiosity/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.myanmar.tmn.padc_f_tmn_curiosity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by msi on 11/29/2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
